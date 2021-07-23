4bf86a1c33a6a93b88f2cac6803264e682455662
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2021-07-23 12:12:41 -0600

    Update reader.py

--- reader/reader.py
+++ reader/reader.py
@@ -4,12 +4,12 @@ Contributor: Thomas Martin
 This has been used from GemGis, more specifically the raster toolkit.
 
 https://github.com/cgre-aachen/gemgis/blob/main/gemgis/raster.py
+The contributors for GemGis: Alexander Jüstel, Arthur Endlein Correia, Florian Wellmann
 
 leapfrog_msh_reader is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 
 leapfrog_msh_reader is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
