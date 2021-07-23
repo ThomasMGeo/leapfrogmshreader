f3db7944f89325fd728a523841b202b3ffcfadd2
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2021-07-23 14:05:35 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'leapfrog_msh_reader',        
   packages = ['leapfrog_msh_reader'],  
+  #package_dir={"":"src"},
   version = '0.1',      
   license='LGPL 3.0',       
   description = 'A simple reader of leapfrog mesh files, without onerous installation requirements.',  
   author = 'Thomas Martin',                  
   author_email = 'tpm319@gmail.com',      
-  url = 'https://github.com/ThomasMGeo/leapfrog_msh_reader',   
-  download_url = 'https://github.com/ThomasMGeo/leapfrog_msh_reader.git',  
+  url = 'https://github.com/ThomasMGeo/leapfrog_msh_reader',     
   keywords = ['leapfrog', 'mesh', '.msh'],   
   install_requires=[            
           'os',
@@ -19,8 +19,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      
     'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: LGPL 3.0',   # Again, pick a license
-    'Programming Language :: Python :: 3.6+',
+    'Topic :: Software Development :: Build Tools',  
+    'Programming Language :: Python :: 3.6',
   ],
 )
